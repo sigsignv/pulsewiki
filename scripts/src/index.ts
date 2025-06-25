2e75b984826cd6ffc22250aa3d5f8c47a5ea6320
Sigsign <sig@signote.cc>
2025-06-25 21:58:03 +0900

    Split updateCounterItems() from skin/main.js

--- scripts/src/index.ts
+++ scripts/src/index.ts
@@ -1,5 +1,7 @@
 import { autofillCommentName } from "./comment";
+import { updateCounterItems } from "./counter";
 
 document.addEventListener("DOMContentLoaded", () => {
   autofillCommentName();
+  updateCounterItems();
 });
