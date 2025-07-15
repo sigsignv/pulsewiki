68911779e59b6aa7cb85d3f338ed78f7c2eca459
Sigsign <sig@signote.cc>
2025-07-15 21:08:38 +0900

    Add tests for filename encoding logic

--- tests/Compat/FuncTest.php
+++ tests/Compat/FuncTest.php
@@ -19,4 +19,29 @@ class FuncTest extends TestCase
         define('PKWK_UTF8_ENABLE', 1);
         $this->assertEquals(get_preg_u(), 'u');
     }
+
+    public function testEncodeAscii()
+    {
+        $this->assertEquals('', encode(''));
+        $this->assertEquals('46726F6E7450616765', encode('FrontPage'));
+        $this->assertEquals('466F726D617474696E6752756C6573', encode('FormattingRules'));
+    }
+
+    public function testEncodeUTF8()
+    {
+        $this->assertEquals('E38386E382B9E38388', encode('テスト'));
+        $this->assertEquals('E697A5E69CACE8AA9E', encode('日本語'));
+    }
+
+    public function testEncodeEUCJP()
+    {
+        $this->assertEquals(
+            'A5C6A5B9A5C8',
+            encode(mb_convert_encoding('テスト', 'EUC-JP', 'UTF-8'))
+        );
+        $this->assertEquals(
+            'C6FCCBDCB8EC',
+            encode(mb_convert_encoding('日本語', 'EUC-JP', 'UTF-8'))
+        );
+    }
 }
